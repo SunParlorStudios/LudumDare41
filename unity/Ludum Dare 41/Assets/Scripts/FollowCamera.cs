87ebf5927b2b4949cac990eb3eab4314bdc7d864
Daniel Konings <konings_304@hotmail.com>
2018-04-21 07:24:25 +0200

    - FollowCamera added

---
+++ unity/Ludum Dare 41/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+  public Transform target;
+  public Vector3 offset;
+
+  [Range(0.0f, 1.0f)]
+  public float stepSize;
+  
+	void Start()
+  {
+		
+	}
+	
+	void FixedUpdate()
+  {
+    if (target == null)
+    {
+      return;
+    }
+
+    Vector3 t = target.position + offset;
+
+    transform.position = Vector3.Lerp(transform.position, t, stepSize);
+	}
+}
