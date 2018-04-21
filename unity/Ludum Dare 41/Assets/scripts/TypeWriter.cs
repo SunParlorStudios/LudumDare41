2f0e483067023e25a16094c5229fe9b3cb1d8522
Riko Ophorst <riko_ophorst@hotmail.com>
2018-04-21 05:41:34 +0200

    Type Writer

---
+++ unity/Ludum Dare 41/Assets/scripts/TypeWriter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypeWriter : MonoBehaviour
+{
+    public delegate void WordListener(string str);
+
+    private string currentInput;
+    private Dictionary<string, WordListener> words;
+
+	void Start ()
+    {
+        words = new Dictionary<string, WordListener>();
+    }
+	
+	void Update ()
+    {
+        if (Input.inputString != "")
+        {
+            currentInput += Input.inputString;
+
+            if(words.ContainsKey(currentInput))
+            {
+                words[currentInput].Invoke(currentInput);
+
+                currentInput = "";
+            }
+        }
+    }
+
+    void RegisterWord(string word, WordListener listener)
+    {
+        if (words.ContainsKey(word))
+        {
+            words[word] += listener;
+        }
+        else
+        {
+            words.Add(word, listener);
+        }
+    }
+}
