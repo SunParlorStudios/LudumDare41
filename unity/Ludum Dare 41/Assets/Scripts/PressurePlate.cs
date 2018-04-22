7fe0f2bb5f6bb430f072caf72993d46556978826
Riko Ophorst <riko_ophorst@hotmail.com>
2018-04-22 12:49:15 +0200

    Pressure Plates animations

--- unity/Ludum Dare 41/Assets/Scripts/PressurePlate.cs
+++ unity/Ludum Dare 41/Assets/Scripts/PressurePlate.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PressurePlateState
+{
+  kUp,
+  kDown
+}
+
 public class PressurePlate : MonoBehaviour
 {
+  public PressurePlateState state
+  {
+    get { return state_; }
+  }
+
+  private PressurePlateState state_;
+  private Animator animator_;
+
 	void Start()
   {
-		
+    animator_ = GetComponent<Animator>();
 	}
 	
 	void Update()
   {
-		
+		if (Input.GetKeyDown(KeyCode.Alpha0))
+    {
+      animator_.SetInteger("State", 0);
+    }
+
+    if (Input.GetKeyDown(KeyCode.Alpha9))
+    {
+      animator_.SetInteger("State", 1);
+    }
 	}
 }
