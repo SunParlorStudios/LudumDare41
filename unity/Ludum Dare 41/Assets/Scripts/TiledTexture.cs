18c1e8516f754d2f38f525d71fd522647d2baa34
Daniel Konings <konings_304@hotmail.com>
2018-04-22 21:27:59 +0200

    - Added tiled walls - Added a background to Level02

---
+++ unity/Ludum Dare 41/Assets/Scripts/TiledTexture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledTexture : MonoBehaviour
+{
+  public Material tiledMaterial;
+  public Vector2 tiling;
+
+  void Start()
+  {
+    Material mat = new Material(tiledMaterial);
+    GetComponent<MeshRenderer>().material = mat;
+    Vector3 scale = transform.localScale;
+
+    scale.x *= tiling.x;
+    scale.y *= tiling.y;
+
+    mat.mainTextureScale = scale;
+  }
+}
