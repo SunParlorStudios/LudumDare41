05e931d1821f5bc9d304d8a7b5d569be49067b1e
Riko Ophorst <riko_ophorst@hotmail.com>
2018-04-23 14:23:00 +0200

    Precursor Cells & Power Orbs

---
+++ unity/Ludum Dare 41/Assets/Scripts/Collectible.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+  private Animator animator_;
+  private AudioSource audio_;
+  private MeshRenderer[] renderer_;
+
+	void Start ()
+  {
+    audio_ = GetComponent<AudioSource>();
+    renderer_ = GetComponentsInChildren<MeshRenderer>();
+    animator_ = GetComponent<Animator>();
+	}
+	
+	void Update ()
+  {
+    
+	}
+
+  void OnTriggerEnter2D(Collider2D collider)
+  {
+    if (collider.tag == "Player")
+    {
+      audio_.Play();
+      animator_.SetTrigger("Pickup");
+    }
+  }
+
+  void PickupAnimationComplete()
+  {
+    Destroy(gameObject);
+  }
+}
