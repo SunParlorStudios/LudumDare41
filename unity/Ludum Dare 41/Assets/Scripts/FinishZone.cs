0bff5ac60157e37a1b718d3cfae18b8a01852d6c
Riko Ophorst <riko_ophorst@hotmail.com>
2018-04-23 21:12:44 +0200

    dope

--- unity/Ludum Dare 41/Assets/Scripts/FinishZone.cs
+++ unity/Ludum Dare 41/Assets/Scripts/FinishZone.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class FinishZone : MonoBehaviour
 {
+  public Transform doorPoint;
+
   private Game game_;
-
-  public GameObject particleEffect;
-  public Transform particleSpawnPoint;
-
+  private Player player_;
+  private Animator animator_;
+  private bool playerGettingAnimated_ = false;
+  
   void Start()
   {
     game_ = GameObject.FindGameObjectWithTag("GameController").GetComponent<Game>();
+    player_ = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    animator_ = GetComponent<Animator>();
   }
 
   void Update()
   {
-
+    if (playerGettingAnimated_)
+    {
+      player_.transform.position = Vector3.Lerp(player_.transform.position, doorPoint.position, 0.01f);
+    }
   }
 
   void OnTriggerEnter2D(Collider2D collider)
@@ -25,9 +32,12 @@ public class FinishZone : MonoBehaviour
     {
       game_.reachedFinish = true;
 
-      if (particleEffect != null && game_.HasMetWinConditions())
+      if (game_.HasMetWinConditions())
       {
-        Instantiate(particleEffect, particleSpawnPoint);
+        player_.enabled = false;
+        player_.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        playerGettingAnimated_ = true;
+        animator_.SetTrigger("Finished");
       }
     }
   }
