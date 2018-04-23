a0d2549e5e98b1351932a187b88c4abbe9c1054c
Daniel Konings <konings_304@hotmail.com>
2018-04-23 21:39:24 +0200

    - Player death and explosion sound effects

---
+++ unity/Ludum Dare 41/Assets/Scripts/OnHitSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHitSound : MonoBehaviour
+{
+  AudioSource[] sources_;
+
+  void Start()
+  {
+    sources_ = GetComponents<AudioSource>();
+  }
+
+  void OnCollisionEnter2D(Collision2D collision)
+  {
+    foreach (AudioSource s in sources_)
+    {
+      s.Play();
+    }
+  }
+}
